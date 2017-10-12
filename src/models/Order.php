fb9b6800eccaa507ddc4aa1f4ae54bcbb0fd9f9a
s.asad <s.asad@nextgeni.com>
2017-10-12 14:55:49 +0500

    check

--- src/models/Order.php
+++ src/models/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
-{
+{ //check
 	  use SoftDeletes;
     protected $table = 'order';
     protected $primaryKey = "order_id";
